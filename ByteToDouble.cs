4ad3db87b257b3abd74e23d58597f52d8a5d6460
Blake Judson <b.judson@hotmail.com>
2021-12-15 13:22:11 +0100

    Senior Design Program
    This was a short program that I wrote for my senior design project in 2018. It converts a number in the form of a byte to a double as the name implies.

---
+++ ByteToDouble.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string number = "1.0023";
+            double answer = convertString(number);
+            Console.WriteLine(answer);
+        }
+        static double convertString(string number)
+        {
+            int i = 0;
+            int before = 0;
+            double answer = 0;
+            while (number[i] != '.')
+            {
+                before++;
+                i++;
+            }
+            i = 0;
+            while (before > 0)
+            {
+                answer += (number[i] - 48) * (Math.Pow((10), before - 1));
+                before--;
+                i++;
+            }
+            int after = 1;
+            i++;
+            while (i < number.Length)
+            {
+                if (number[i] == '0')
+                {
+                    i++;
+                    after++;
+                    continue;
+                }
+                answer += (number[i] - 48) * (Math.Pow((10), -after));
+                after++;
+                i++;
+            }
+            return answer;
+        }
+    }
+}
